00fb87b30a422e839a9cd846b469e50e6cec33da
Zakariyah Toyer <tyrzak001@myuct.ac.za>
2018-03-14 13:03:14 +0200

    I've updated the lexer to reflect where a syntax error occurs

--- lexer.py
+++ lexer.py
@@ -10,21 +10,28 @@ def open_file(filename):
 def run():
 	open_file(argv[1])
 	data = open_file(argv[1])
-	t_list = lex(data)
-	for i in range (len(t_list)):
-		print('line ', i+1, t_list[i])
-def lex(data):
+	data = data.split('\n')
+	tokenized = []
 
+	for i in range(len(data)-1):
+		tokenized.append(lex(data[i], (i+1)))
+
+	for i in range(len(tokenized)):
+		print('line ', i, tokenized[i])
+
+
+
+def lex(data, line_num):
 	pos = 0
 	token_list = []
 	str = ''
 	#state = 1 means everything we're looking at is part of string
 	state = 0
-	sub_list = []
+	
 	while pos < len(data):
 		match = None
 		token_exprs = tokens.token_exprs
-		for i in range(len(token_exprs)):
+		for i in range(len(token_exprs)-2):
 			pattern, tag = token_exprs[i]
 			regex = re.compile(pattern)
 			match = regex.match(data, pos)
@@ -41,28 +48,26 @@ def lex(data):
 								regex = re.compile(pattern)
 								match = regex.match(data, pos+1)
 								str = match.group(0)
-								sub_list.append((str, tag))
+								token_list.append((str, tag))
 								pos = match.end(0)
 								break
 							if state == 0: 
+								
 								break
 					if(tag == 'INT'):
 						#we want to strip leading zeros except if its '0'
 						if(len(str) > 1):
 							str = str.lstrip('0')
-					if (tag == 'ENDL'):
-						token_list.append(sub_list)
-						sub_list = []
 					#create a token - text and tag tuple
 					else:
 						token = (str, tag)
-						sub_list.append(token)
+						token_list.append(token)
 				break
 		if not match:
-			print('Illegal character: %s' % data[pos], data[pos:])
+			print('Illegal character in line',line_num,':', data,'at position %d' % pos)
 			sys.exit()
 		else:
-				pos = match.end(0)
+			pos = match.end(0)
 	return token_list
 	
 
